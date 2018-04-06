53562d9b3c1aa68e3bb1cd5c25ddfd3a7a728c21
Jack <37362585+Zjack1@users.noreply.github.com>
2018-04-05 22:45:59 -0700

    Update OCR_ANN.py

--- OCR_ANN.py
+++ OCR_ANN.py
@@ -23,25 +23,27 @@ def compute_accuracy(v_xs, v_ys):
     return result
 
 # define placeholder for inputs to network
-xs = tf.placeholder(tf.float32,[None, 784]) # 28x28
-ys = tf.placeholder(tf.float32,[None, 10])
+xs = tf.placeholder(tf.float32, [None, 784]) # 28x28
+ys = tf.placeholder(tf.float32, [None, 10])
 
 # add output layer
-prediction = add_layer(xs,784,10,activation_function=tf.nn.softmax)
+prediction = add_layer(xs, 784, 10,  activation_function=tf.nn.softmax)
 
 # the error between prediction and real data
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
                                               reduction_indices=[1]))       # loss
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.8).minimize(cross_entropy)
 
 sess = tf.Session()
-
+# important step
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
 init = tf.global_variables_initializer()
 sess.run(init)
 
-for i in range(50000):
+for i in range(10000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
-    if i % 500 == 0:
+    if i % 50 == 0:
         print(compute_accuracy(
             mnist.test.images, mnist.test.labels))
