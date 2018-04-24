0f10c3ab4339b43c2b3d1567718a242e86bc6980
Jack <37362585+Zjack1@users.noreply.github.com>
2018-04-24 07:45:06 -0500

    Update test_CNN.py

--- test_CNN.py
+++ test_CNN.py
@@ -1,9 +1,8 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import input_data
-
-mnist = input_data.read_data_sets('data/', one_hot=True)
+from tensorflow.examples.tutorials.mnist import input_data
+mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
 trainimg   = mnist.train.images
 trainlabel = mnist.train.labels
 testimg    = mnist.test.images
@@ -18,7 +17,7 @@ weights  ={
             'wd1': tf.Variable(tf.random_normal([7*7*128, 1024], stddev=0.1)),
             'wd2': tf.Variable(tf.random_normal([1024, n_output], stddev=0.1))
             }
-    biases   = {
+biases   = {
             'bc1': tf.Variable(tf.random_normal([64], stddev=0.1)),
             'bc2': tf.Variable(tf.random_normal([128], stddev=0.1)),
             'bd1': tf.Variable(tf.random_normal([1024], stddev=0.1)),
@@ -68,7 +67,7 @@ y = tf.placeholder(tf.float32, [None, n_output])
 keepratio = tf.placeholder(tf.float32)
 
 _pred = conv_basic(x, weights, biases, keepratio)['out']
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(_pred, y))
+cost = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=_pred, labels=y))
 ptm = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 _corr = tf.equal(tf.argmax(_pred,1), tf.argmax(y,1)) 
 accr = tf.reduce_mean(tf.cast(_corr, tf.float32)) 
@@ -77,7 +76,7 @@ print ("GRAPH READY")
 
 sess = tf.Session()
 sess.run(init)
-training_epochs = 15
+training_epochs = 45
 batch_size      = 16
 display_step    = 1
 for epoch in range(training_epochs):
@@ -88,7 +87,7 @@ for epoch in range(training_epochs):
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
         # Fit training using batch data
-        sess.run(optm, feed_dict={x: batch_xs, y: batch_ys, keepratio:0.7})
+        sess.run(ptm, feed_dict={x: batch_xs, y: batch_ys, keepratio:0.7})
         # Compute average loss
         avg_cost += sess.run(cost, feed_dict={x: batch_xs, y: batch_ys, keepratio:1.})/total_batch
     
@@ -96,7 +95,7 @@ for epoch in range(training_epochs):
     if epoch % display_step == 0: 
         print ("Epoch: %03d/%03d cost: %.9f" % (epoch, training_epochs, avg_cost))
         train_acc = sess.run(accr, feed_dict={x: batch_xs, y: batch_ys, keepratio:1.})
-        print (" Training accuracy: %.3f" % (train_acc))
+        print ("Training accuracy: %.3f" % (train_acc))
         #test_acc = sess.run(accr, feed_dict={x: testimg, y: testlabel, keepratio:1.})
         #print (" Test accuracy: %.3f" % (test_acc))
     
