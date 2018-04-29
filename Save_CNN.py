e251ee33e47c4c3a88740493a8e1a2a0d2c5c220
Jack <37362585+Zjack1@users.noreply.github.com>
2018-04-29 08:10:29 -0500

    Update Save_CNN.py

--- Save_CNN.py
+++ Save_CNN.py
@@ -2,7 +2,7 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import input_data"
+from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('data/', one_hot=True)
 trainimg   = mnist.train.images
